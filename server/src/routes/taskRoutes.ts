4df029898827554733fccf530137587ff61e79f8
Priyanshi9891 <priyumaurya02@gmail.com>
2026-06-29 19:39:23 +0530

    Fix the code of taskRoute.ts

--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -1,27 +1,4 @@
-// import express from "express";
-// import {
-//   createTask,
-//   getTasks,
-//   markTaskComplete,
 
-//   deleteTask,
-// } from "../controllers/taskController";
-
-// const router = express.Router();
-
-// // CREATE
-// router.post("/tasks", createTask);
-
-// // READ
-// router.get("/tasks", getTasks);
-
-// // UPDATE STATUS
-// router.patch("/tasks/:id/complete", markTaskComplete);
-
-// // DELETE
-// router.delete("/tasks/:id", deleteTask);
-
-// export default router;
 import express from "express";
 import {
   createTask,
@@ -34,16 +11,12 @@ import { validateTask } from "../middleware/validateTask";
 
 const router = express.Router();
 
-// CREATE TASK (with middleware)
 router.post("/tasks", validateTask, createTask);
 
-// READ
 router.get("/tasks", getTasks);
 
-// UPDATE
 router.patch("/tasks/:id/complete", markTaskComplete);
 
-// DELETE
 router.delete("/tasks/:id", deleteTask);
 
 export default router;
